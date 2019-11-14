13a595042df2eed348c4b144b2e9f203b5140613
Ansh Gandhi <agandhi543@gmail.com>
2019-11-14 13:19:04 -0500

    this won't work no matter what

--- src/sum.js
+++ src/sum.js
@@ -1,50 +1,58 @@
 const readlineSync = require("readline-sync");
+
 const MIN = Number.MIN_SAFE_INTEGER
 const MAX = Number.MAX_SAFE_INTEGER
-
-
-
+let sum = 0
+i = 0
+console.log("");
 do {
-lowerBound = Number(readlineSync.question("\nLower bound: "));
-}
-while (((lowerBound <= MIN) && (lowerBound >= MAX))  && (!Number.isInteger(upperBound)))
-
-  if ((lowerBound >= MIN) && (lowerBound <= MAX))  {
-  let one = lowerBound
-}
-let sum = lowerBound
-
-do {
+lowerBound = Number(readlineSync.question("Lower bound: "));
 upperBound = Number(readlineSync.question("Upper bound: "));
-}
-while (((lowerBound <= MIN) && (lowerBound >= MAX)) && (!Number.isInteger(upperBound)))
-  if ((upperBound >= MIN) && (upperBound <= MAX))  {
-  let sum1 = upperBound
-}
-let sum1 = upperBound
-
-
-do {
 lowerBound2 = Number(readlineSync.question("Lower bound: "));
-}
-while (((lowerBound2 <= MIN) && (lowerBound2 >= MAX)) && (!Number.isInteger(upperBound)))
-  if ((lowerBound2 >= MIN) && (lowerBound2 <= MAX)) {
-  let sum2 = lowerBound2
-}
-let sum2 = lowerBound2
-
-
-do {
 upperBound2 = Number(readlineSync.question("Upper bound: "));
 }
-while (((upperBound <= MIN) && (upperBound >= MAX)) && (!Number.isInteger(upperBound)))
-  if ((upperBound >= MIN) && (upperBound <= MAX)) {
-  let sum3 = lowerBound
+while (lowerBound >= MIN &&
+       lowerBound <= MAX &&
+       upperBound >= MIN &&
+       upperBound <= MAX &&
+       !Number.isInteger(lowerBound) &&
+       !Number.isInteger(upperBound) &&
+       lowerBound > upperBound &&
+       lowerBound2 >= MIN &&
+              lowerBound2 <= MAX &&
+              upperBound2 >= MIN &&
+              upperBound2 <= MAX &&
+              !Number.isInteger(lowerBound2) &&
+              !Number.isInteger(upperBound2) &&
+              lowerBound2 > upperBound2);
+
+for (let i = lowerBound; i <= upperBound; i++) {
+   if (i % 2 === 0) {
+      for (i = lowerBound; i <= upperBound; i+=2) {
+        sum = i + sum
+      }
+    }
+        if (i % 2 !== 0) {
+            for (i = lowerBound + 1; i <= upperBound; i+=2) {
+     sum = i + sum
 }
-let sum3 = upperBound2
+  }
+}
 
+for (let i = lowerBound2; i <= upperBound2; i++) {
+   if (i % 2 === 0) {
+      for (i = lowerBound2; i <= upperBound2; i+=2) {
+        sum2 = i + sum
+      }
+    }
+        if (i % 2 !== 0) {
+            for (i = lowerBound2 + 1; i <= upperBound2; i+=2) {
+     sum2 = i + sum
+}
+  }
+}
 
+let sum3 = (sum+sum2)
+let finalSum = sum3.toLocaleString('en');
 
-let final = (sum + sum1 + sum2 + sum3)
-
-console.log("\n" + final + ".");
+console.log("\n" + finalSum + ".");
