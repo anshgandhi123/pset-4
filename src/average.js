f4931767edd8fede6cddec006fd1da6bd1f4dd88
me <->
2019-11-20 20:53:51 -0500

    it still doesn't work

--- src/average.js
+++ src/average.js
@@ -3,15 +3,17 @@ const min = 0
 const MAX = Number.MAX_SAFE_INTEGER
 console.log("");
 let sum = 0
-x = 0
+average = 0
 do {
   integer = Number(readlineSync.question("Non-negative integer: "));
-}  while(integer > MAX || Number.isNaN(integer) || !Number.isInteger(integer) || integer >= 0)
+}
+while (integer > MAX || Number.isNaN(integer) || !Number.isInteger(integer) || integer >= 0)
 
 if (integer !== -1) {
-  integer = integer + sum
-  x = x + 1
+  sum = integer + sum
+  average = average + 1
 }
-answer = integer/x
-
-console.log("\n" + answer + "\n")
+answer = (sum/average)
+let final = Math.round(100*answer)/(100)
+let finalAnswer = final.toLocaleString("en", {minimumFractionDigits:3, maximumFractionDigits:3})
+console.log("\n" + finalAnswer + "\n")
