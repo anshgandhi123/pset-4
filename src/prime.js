80a2f37ca8a3628b67fe6d06571ea3ba73a6bb42
me <->
2019-11-24 18:37:54 -0500

    final commit

--- src/prime.js
+++ src/prime.js
@@ -1,13 +1,13 @@
 const readlineSync = require("readline-sync");
 const MAX = Number.MAX_SAFE_INTEGER
-const min = 0
+const MIN = 0
 let value = 2
 
 console.log("");
 
 do {
   integer = Number(readlineSync.question("Non-negative integer: "));
-}  while(integer < min || integer > MAX || Number.isNaN(integer) || !Number.isInteger(integer))
+}  while(integer < MIN || integer > MAX || Number.isNaN(integer) || !Number.isInteger(integer))
 
 
 while (value < integer) {
@@ -22,7 +22,7 @@ while (value < integer) {
       break;
     }
   }
-  if (integer == 1) {
+  if (integer == 1 || integer == 0) {
     console.log("\nNot prime.\n")
   }
     else if (value >= integer) {
