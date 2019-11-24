28a5260720f3421668a715269d1b21588e7df83d
me <->
2019-11-24 18:39:30 -0500

    final commit

--- src/average.js
+++ src/average.js
@@ -1,17 +1,17 @@
 const readlineSync = require("readline-sync");
-const min = 0
+const MIN = 0
 const MAX = Number.MAX_SAFE_INTEGER
 console.log("");
 let sum = 0
 average = 0
 do {
   integer = Number(readlineSync.question("Non-negative integer: "));
-  if (integer >= 0 && integer < MAX && !Number.isNaN(integer)) {
+  if (integer >= MIN && integer < MAX && !Number.isNaN(integer) && integer % 1 == 0) {
     sum = integer + sum
     average = average + 1
   }
 }
-while ((integer >= 0 && integer < Number.MAX_SAFE_INTEGER &&  integer % 1 == 0) || Number.isNaN(integer))
+while ((integer >= 0 && integer < MAX &&  integer % 1 == 0) || Number.isNaN(integer) || average == 0)
 let answer = (sum/average)
 let final = Math.round(100*answer)/(100)
 let finalAnswer = final.toLocaleString("en", {minimumFractionDigits:3, maximumFractionDigits:3})
